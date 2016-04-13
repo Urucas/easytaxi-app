7a849e2f3456db593efe801ea0cbd980bd972d08
vrunoa <vruno.alassia@gmail.com>
2016-04-13 00:07:06 -0300

    tray app implementation

---
+++ public/js/app.js
@@ -0,0 +1,18 @@
+var body   = document.getElementsByTagName("body")[0];
+var progress = document.getElementsByTagName("progress")[0];
+var closeBtt = document.querySelector("button.close");
+closeBtt.onclick = window.close;
+var MAX = 100;
+progress.setAttribute("max", MAX);
+var interval = setInterval(updateProgress, 500);
+function updateProgress() {
+  progress.value = progress.value + 5 <= MAX ? progress.value + 5 : 0;
+}
+var iframe = document.createElement("iframe");
+iframe.onload = function() {
+  clearInterval(interval);
+  progress.value = MAX;  
+}
+iframe.src = "https://webapp.easytaxi.com";
+iframe.setAttribute("frameborder",  0);
+body.appendChild(iframe);
